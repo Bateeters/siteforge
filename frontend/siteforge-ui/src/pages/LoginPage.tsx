1ddf58240a9629530f8ed2a512c3c79a0ed7236f
Brian Teeters <BrianTeetersDesign@gmail.com>
2026-06-04 19:04:26 -0400

    Implement login page state and navigation

--- frontend/siteforge-ui/src/pages/LoginPage.tsx
+++ frontend/siteforge-ui/src/pages/LoginPage.tsx
@@ -1,6 +1,53 @@
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+
 function LoginPage() {
+    const [loginData, setLoginData] = useState({
+        email: "",
+        password: ""
+    });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+
+        setLoginData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+    };
+
+    const navigate = useNavigate();
+
+    const handleLogin = (e: React.SubmitEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log(loginData);
+        navigate("/dashboard");
+    };
+
     return (
-        <h1>Login Page</h1>
+        <>
+            <h1>SiteForge</h1>
+            <form onSubmit={handleLogin}>
+                <label>Email:</label>
+                <input 
+                    type="email" 
+                    placeholder="Email"
+                    name="email" 
+                    value={loginData.email} 
+                    onChange={handleChange}/>
+                <label>Password:</label>
+                <input 
+                    type="password" 
+                    placeholder="Password" 
+                    name="password"
+                    value={loginData.password}
+                    onChange={handleChange}/>
+                
+                <button type="submit">Login</button>
+            </form>
+            <p>Need an account?</p>
+            <Link to="/register">Create Account</Link>
+        </>
     );
 }
 
