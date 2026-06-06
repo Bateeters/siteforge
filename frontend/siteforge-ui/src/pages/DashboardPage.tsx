2eb83fc1aa8c408da2f0c52ab24771cdb64321c6
Brian Teeters <BrianTeetersDesign@gmail.com>
2026-06-06 12:55:01 -0400

    implement dashboard to render website cards from data

--- frontend/siteforge-ui/src/pages/DashboardPage.tsx
+++ frontend/siteforge-ui/src/pages/DashboardPage.tsx
@@ -1,6 +1,38 @@
+import { Link } from "react-router-dom";
+
 function DashboardPage() {
+    const websites = [
+        { 
+            id: 1,
+            name: "Porfolio Website",
+            lastUpdated: "2026-05-15"
+        },
+        {
+            id: 2,
+            name: "Food Chain Rumble",
+            lastUpdated: "2026-04-11"
+        },
+        {
+            id: 3,
+            name: "Test Site",
+            lastUpdated: "2026-03-20"
+        }
+    ]
     return (
-        <h1>Dashboard Page</h1>
+        <>
+            <h1>Welcome (FirstName)</h1>
+            <button>Create Website</button>
+
+            <h2>Your Websites</h2>
+            {/* Each card should link to "/editor/:siteid" */}
+            {websites.map((site) => (
+                <div key={site.id}>
+                    <h3>{site.name}</h3>
+                    <h5>Last Updated: {site.lastUpdated}</h5>
+                    <Link to={`/editor/${site.id}`}>Open</Link>
+                </div>
+            ))}
+        </>
     );
 }
 
