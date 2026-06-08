5cca48433fecc3ed72953d05953e921bbf3a653e
Brian Teeters <BrianTeetersDesign@gmail.com>
2026-06-08 11:12:28 -0400

    updated basic boilerplate EditorPage

--- frontend/siteforge-ui/src/pages/EditorPage.tsx
+++ frontend/siteforge-ui/src/pages/EditorPage.tsx
@@ -1,6 +1,15 @@
 function EditorPage() {
     return (
-        <h1>Editor Page</h1>
+        <>
+            <div><p>Top Toolbar</p></div>
+            <div>
+                <div>
+                    <p>Content Area</p>
+                    <h1>Website Title</h1>
+                </div>
+                <div><p>Editor Side Panel</p></div>
+            </div>
+        </>
     );
 }
 
