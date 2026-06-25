6d88895d6e3388a3ae57110fbc83f29d585348f8
Brian Teeters <BrianTeetersDesign@gmail.com>
2026-06-25 17:43:46 -0400

    added comments explaining column type

--- frontend/siteforge-ui/src/types/column.ts
+++ frontend/siteforge-ui/src/types/column.ts
@@ -6,5 +6,10 @@ export type Column = {
         id: number;
         type: string;
         props?: Record<string, unknown>;
+        // ? -> optional
+        // Record<string, unknown> -> an object with keys that are strings, and values that can be anything
+            // we use "unknown" because we don't know yet what the values are going to be so we create a "flexible container"
+            // we do NOT use "any" because it basically turns TypeScript off (everything works and TS doesn't help you... That's bad!)
+        // in the future, "props" will hold the component object.
     };
 };
